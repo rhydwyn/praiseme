109f7ae3dd8c37001b7d572c72b841a0f64ce6f4
Rhydwyn <rhydwyn.beta@gmail.com>
2018-11-21 15:27:49 +1100

    Tutorial repository

---
+++ R/praise.r
@@ -0,0 +1,13 @@
+#'@title Praise someone
+#'@description dole out the praise ❤️
+#'@param name Specifies the recipient of praise
+#'@examples praise("John")
+#' @export
+
+
+
+praise <- function(name = "Rhydwyn"){
+
+  paste0("you're the best ", name, '! ❤️")
+
+}
